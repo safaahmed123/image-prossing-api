ba94f2402aa2ed13745cd400addb3aa3bdf332e7
unknown <safaelzuhry@gmail.com>
2022-04-19 08:17:17 +0200

    add middleware for loggin ,security

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import express, { Application, Request, Response } from 'express'
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from "express-rate-limit";
 
 const app:Application=express();
 
 const port=3000;
+app.use(express.json);
+
+
+app.use(morgan('common'));
+app.use(helmet());
+app.post('/',(req:Request ,res :Response)=>{
+    res.json({
+        message:'hello from post',
+        data:req.body,
+    });
+
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.json({
