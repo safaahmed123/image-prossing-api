8e56da058ac11f1ee5cc49ab9b10705026014e79
unknown <safaelzuhry@gmail.com>
2022-04-19 08:54:58 +0200

    handling error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan';
 import helmet from 'helmet';
 import rateLimit from "express-rate-limit";
+import errorMiddleware from './middleware/error.middleware';
 
 const app:Application=express();
 
@@ -11,6 +12,18 @@ app.use(express.json);
 
 app.use(morgan('common'));
 app.use(helmet());
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+message:"too many requst "
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+
 app.post('/',(req:Request ,res :Response)=>{
     res.json({
         message:'hello from post',
@@ -20,9 +33,19 @@ app.post('/',(req:Request ,res :Response)=>{
 })
 
 app.get('/', (req: Request, res: Response) => {
+    throw new Error('error exist');
     res.json({
       message: 'Hello World 🌍'
-    })
+    });
+  });
+
+  app.use(errorMiddleware);
+
+  app.use((_req:Request,res:Response)=>{
+      res.status(404).json({
+          message:
+          'ohh you are lost '
+      });
   });
 
 app.listen(port, () => {
---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,17 @@
+import {Response,Request, NextFunction } from "express";
+import Error  from "../interfaces/error.interface";
+const errorMiddleware= (
+    error:Error,
+    req:Request,
+    res:Response,
+    next:NextFunction
+)=>{
+    const status=error.status||500;
+    const message=error.message||'somthing when wrong';
+    
+    res.status(status).json({status,message});
+};
+
+export default errorMiddleware;
+    
+    
